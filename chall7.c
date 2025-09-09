819ad2e7aa012e25b2a1caea023317fea6aa0f05
Wassim-RF <wassimrifi0313@gmail.com>
2025-09-09 15:07:33 +0200

    #7

---
+++ chall7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+int main() {
+    char ch[50];
+    int  i;
+    printf("Entrer un chaine : ");
+    fgets(ch ,  50, stdin);
+    ch[strcspn(ch , "\n")] = '\0' ;
+    for (i = 0 ; ch[i] != '\0' ; i++) {
+        ch[i] = toupper(ch[i]);
+    }
+    printf("%s" , ch);
+    return 0;
+}
